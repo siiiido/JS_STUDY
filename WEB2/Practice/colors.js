7ad30a622c62a6d0dd0a1acc1b9fe14a56d3a46a
SangHan Song <discegaudere24.7@gmail.com>
2020-09-23 23:04:14 +0900

    done

---
+++ WEB2/Practice/colors.js
@@ -0,0 +1,34 @@
+var Links={
+  setColor:function(color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = color;
+      i++;
+    }
+  }
+}
+
+var Body = {
+  setColor:function(color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+
+function nightDayHandler(self) {
+  var target = document.querySelector('body');
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black');
+    self.value = 'day';
+    Body.setColor('white');
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+    Links.setColor('tomato');
+  }
+}
