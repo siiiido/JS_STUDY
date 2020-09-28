581459ed7ab284a42d7004aa548f8050c95141b6
SangHan Song <discegaudere24.7@gmail.com>
2020-09-28 23:05:07 +0900

    add explaination

--- Nomad/ChromeApp/clock.js
+++ Nomad/ChromeApp/clock.js
@@ -1,4 +1,14 @@
+// <함수 설명>
+/*
+querySelector : 문서 내 첫 번쨰 element를 반환,
+일치하는 요소 없으면 null반환
+예를 들면 var el = document.querySelector("div.user-panel.main input[name=login]");
+클래스가 "user-panel main" <div class="user-panel main">안의 이름이"login"인 <input> 중 첫 번째 요소입니다.*/
+// const : 상수를 선언, 상수 값은 재할당할 수 없으며 다시 선언x && 객체에도 같이 적용됨 하지만 객체의 key값은 변경 가능
+
+
 const clockContainer = document.querySelector(".js-clock");
+// clockContainer에서 h1 선택, 즉 js-title을 선택한 것이다
 const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
@@ -11,6 +21,9 @@ function getTime() {
 
   // 10초 보다 작으면 0${second} 실행 아니면 second 실행
   // 삼항 연산자 ->  조건 ? true일때 출력 : false일때 출력
+  /*
+    innerText : 텍스트를 element에 추가할 때 사용
+    시간,분,초를 clockTitle에 추가함*/
   clockTitle.innerText = `${hour < 10 ? `0${hour}` : hour}:${
     minutes < 10 ? `0${minutes}` : minutes}:${
       second < 10 ? `0${second}` : second}`;
@@ -18,7 +31,7 @@ function getTime() {
 
 function init() {
   getTime();
-  // 1000 -> 1초 마다 업데이트되게 해줌
+  // 1000 -> 1초 마다 업데이트되게 해줌->1초마다 함수를 실행시킴
   // setInterval(함수, 실행할 시간 간격)
   setInterval(getTime, 1000);
 }
--- Nomad/ChromeApp/greeting.js
+++ Nomad/ChromeApp/greeting.js
@@ -4,21 +4,41 @@ const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greeting");
 
-// localStorage -> js의 작은 정보들을 저장
+/*
+ localStorage -> js의 작은 정보들을 저장,&& 정수도 문자열로 저장됨
+  저장한 데이터는 브라우저 세션 간에 공유됨
+  그리고 localStorage는 페이즈를 닫아도 데이터 사라지지 않음,
+  하지만 sessionStorage는 페이지 닫으면 데이터 사라짐
+  메서드 -> setItem(키,값), getItem(키)하면 값이 나옴
+  removeItem(키) 키 값의 데이터만 지움,
+  clear() : localStorage 항목의 전체 data제거 */
 
-const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
+const USER_LS = "currentUser", //LS : LocalStorage
+  SHOWING_CN = "showing";      // CN : ClassName
 
+  // 새로고침 했을때 계속 form안에 text값 기억하는 함수
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
-
+/*
+이벤트 : https://developer.mozilla.org/ko/docs/Web/Events
+preventDefault -> prevent:막다, default:기본 이벤트 -> 이벤트를 막다
+  이벤트를 취소할 수 있는 경우 취소함,
+  체크박스 클릭을 취소한다거나, 텍스트에 입력란에 입력되는것을 막는다거나 등등.... */
 function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = input.value;
+  event.preventDefault();  // form에 text입력한걸 계속 남길려고 사용
+  const currentValue = input.value; //이렇게 input에 value값 생성 가능
   paintGreeting(currentValue);
   saveName(currentValue);
 }
+
+/*
+classList는 메소드들을 이용해 html의 class를 추가,삭제....를 함
+  메소드 : add,remove,contain,toggle
+    toggle는 on/off 기능이고 이 함수는 2개의 인자를 받는데
+    처음 인자는 토글링할 class이름 다음 인자는 선택인자로 boolean 타입
+    true면 강제로 class를 추가 false는 강제로 class삭제*/
+
 
 function askForName() {
   form.classList.add(SHOWING_CN);
